cdf34fb4000bfc6d75ee0a3256f03739f635eb26
kensakurai <sakurai.kem@gmail.com>
2017-06-26 22:14:52 +0900

    Modify defaults.py

--- amp_start_blog_post/defaults.py
+++ amp_start_blog_post/defaults.py
@@ -13,3 +13,24 @@ register_setting(
              ),
     append=True,
 )
+
+register_setting(
+    name="AMP_GOOGLE_ADS_CLIENT_ID",
+    description="GOOGLE ADS CLIENT ID for AMP",
+    editable=True,
+    default="",
+)
+
+register_setting(
+    name="AMP_GOOGLE_ADS_SLOT_ID",
+    description="GOOGLE ADS SLOT ID for AMP",
+    editable=True,
+    default="",
+)
+
+register_setting(
+    name="AMP_GOOGLE_TAG_MANGER_ID",
+    description="GOOGLE TAG MANGER ID for AMP",
+    editable=True,
+    default="",
+)
