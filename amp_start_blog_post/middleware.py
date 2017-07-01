073d5923af809110515c9002b33be8e58fc2e7ed
kensakurai <sakurai.kem@gmail.com>
2017-07-01 15:59:02 +0900

    Mod Middleware.py

--- amp_start_blog_post/middleware.py
+++ amp_start_blog_post/middleware.py
@@ -13,22 +13,23 @@ class TemplateForAMPMiddleware(MiddlewareMixin):
     def process_template_response(self, request, response):
 
         url = request.get_full_path()
-        print("########################")
-        print(url)
-        print("########################")
         if url.startswith("/blog/amp/"):
-            if hasattr(response, "template_name"):
-                if not isinstance(response.template_name, Template):
-                    templates = response.template_name
-                    if not isinstance(templates, (list, tuple)):
-                        templates = [templates]
-                    device_templates = []
-                    for template in templates:
-                        device_templates.append("%s/%s" % ("amp_start_blog_post", template))
-                    response.template_name = device_templates
-        else:
-            if hasattr(response, "template_name"):
-                if not isinstance(response.template_name, Template):
-                    templates = templates_for_device(request, response.template_name)
-                    response.template_name = templates
+        # If a device wasn't set via cookie, match user agent.
+            try:
+                request.META["HTTP_USER_AGENT"] = "amp"
+            except KeyError:
+                pass
+            # if hasattr(response, "template_name"):
+            #     if not isinstance(response.template_name, Template):
+            #         templates = response.template_name
+            #         if not isinstance(templates, (list, tuple)):
+            #             templates = [templates]
+            #         device_templates = []
+            #         for template in templates:
+            #             device_templates.append("%s/%s" % ("amp_start_blog_post", template))
+            #         response.template_name = device_templates
+        if hasattr(response, "template_name"):
+            if not isinstance(response.template_name, Template):
+                templates = templates_for_device(request, response.template_name)
+                response.template_name = templates
         return response
