1a41f5344849ad4fdf384910c5a6106d3824f28b
kensakurai <sakurai.kem@gmail.com>
2017-07-06 22:08:57 +0900

    add defaults.py

--- amp_start_blog_post/defaults.py
+++ amp_start_blog_post/defaults.py
@@ -7,8 +7,10 @@ register_setting(
     name="TEMPLATE_ACCESSIBLE_SETTINGS",
     description=("Sequence of setting names available within templates."),
     editable=False,
-    default=("AMP_GOOGLE_ADS_CLIENT_ID",
-             "AMP_GOOGLE_ADS_SLOT_ID",
+    default=("AMP_GOOGLE_ADS_CLIENT_ID_TOP",
+             "AMP_GOOGLE_ADS_SLOT_ID_TOP",
+             "AMP_GOOGLE_ADS_CLIENT_ID_BOTTOM",
+             "AMP_GOOGLE_ADS_SLOT_ID_BOTTOM",
              "AMP_GOOGLE_TAG_MANGER_ID",
              "AMP_TWITTER_ACCOUNT_NAME",
              "AMP_GITHUB_USER_NAME",
@@ -17,14 +19,27 @@ register_setting(
 )
 
 register_setting(
-    name="AMP_GOOGLE_ADS_CLIENT_ID",
+    name="AMP_GOOGLE_ADS_CLIENT_ID_BOTTOM",
     description="GOOGLE ADS CLIENT ID for AMP",
     editable=True,
     default="",
 )
 
 register_setting(
-    name="AMP_GOOGLE_ADS_SLOT_ID",
+    name="AMP_GOOGLE_ADS_SLOT_ID_BOTTOM",
+    description="GOOGLE ADS SLOT ID for AMP",
+    editable=True,
+    default="",
+)
+register_setting(
+    name="AMP_GOOGLE_ADS_CLIENT_ID_TOP",
+    description="GOOGLE ADS CLIENT ID for AMP",
+    editable=True,
+    default="",
+)
+
+register_setting(
+    name="AMP_GOOGLE_ADS_SLOT_ID_TOP",
     description="GOOGLE ADS SLOT ID for AMP",
     editable=True,
     default="",
