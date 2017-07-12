501a2ccd9f5adaed764947283471b2b75a3888f7
kensakurai <sakurai.kem@gmail.com>
2017-07-12 22:18:59 +0900

    Contents HTML 内のampを含むID属性は置換する

--- amp_start_blog_post/templatetags/amp_start_blog_post_tags.py
+++ amp_start_blog_post/templatetags/amp_start_blog_post_tags.py
@@ -13,13 +13,15 @@ def to_amp_html(html):
     Markdown HTML to AMP HTML
     """
     soup = BeautifulSoup(html)
+    for elem in soup.find_all(True, id=lambda x: x and 'amp' in x):
+        elem["id"] = elem.get("id").replace("amp", "accelerated-mobile-pages")
     for img in soup.find_all('img'):
         amp_img = soup.new_tag("amp-img")
         for attr in img.attrs:
             amp_img[attr] = img[attr]
-        src = img.get("src")
-        if src.startwith("//"):
-            src = src.replace("//", "")
+        src = str(img.get("src"))
+        if src.startswith("//"):
+            src = src.replace("//", "https://")
         im = Image.open(requests.get(src, stream=True).raw)
         amp_img["width"] = im.size[0]
         amp_img["height"] = im.size[1]
