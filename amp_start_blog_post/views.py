d0acaa99611431b9511899270495a8a240838666
kemsakuai <sakurai.kem@gmail.com>
2019-05-06 05:08:27 +0900

    Add urllib.parse.quote

--- amp_start_blog_post/views.py
+++ amp_start_blog_post/views.py
@@ -1,10 +1,12 @@
 from django.views import View
 from django.shortcuts import render
 from django.conf import settings as django_settings
+import urllib.parse    
 
 class OptimizedAmpView(View):
     def get(self, request, *args, **kwargs):
-        base_url =  request.path
+        base_url =  urllib.parse.quote(request.path)
         elems = base_url.split("/")
+        
         file_name = elems[len(elems) -2][:100] + ".html"
         return render(request, 'amp_start_blog_post/htmls/' + file_name)
