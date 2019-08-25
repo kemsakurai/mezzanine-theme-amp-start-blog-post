3f869e99b9d18c534964f17442b2df482ec71d40
kemsakuai <sakurai.kem@gmail.com>
2019-08-26 02:51:45 +0900

    Add throwing 404error

--- amp_start_blog_post/views.py
+++ amp_start_blog_post/views.py
@@ -1,12 +1,19 @@
 from django.views import View
-from django.shortcuts import render
-from django.conf import settings as django_settings
-import urllib.parse    
+from django.http import HttpResponse
+from django.http import Http404
+import urllib.parse
+from pathlib import Path
 
 class OptimizedAmpView(View):
     def get(self, request, *args, **kwargs):
         base_url =  urllib.parse.quote(request.path)
         elems = base_url.split("/")
-        
         file_name = elems[len(elems) -2][:100] + ".html"
-        return render(request, 'amp_start_blog_post/htmls/' + file_name)
+        response = HttpResponse(content_type="text/html")
+        directory = str(Path(__file__).resolve().parents[1])
+        try:
+            for line in open(directory + '/amp_start_blog_post/templates/amp_start_blog_post/htmls/' + file_name):
+                response.write(line)
+        except FileNotFoundError:
+            raise Http404("Page does not exist")
+        return response            
